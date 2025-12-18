c7e60956c859e2ee84eb0837d62fb839981f43bb
Danijel Huis <dhuis@4ofthem.eu>
2025-12-18 11:57:05 +0100

    Package update

--- ios/appsdk_v2_flutter_plugin/Package.swift
+++ ios/appsdk_v2_flutter_plugin/Package.swift
@@ -59,9 +59,7 @@ let package = Package(
         .library(name: "appsdk-v2-flutter-plugin", targets: ["appsdk_v2_flutter_plugin"])
     ],
     dependencies: [
-	    //.package(url: "https://github.com/ihreapotheken/IA-SDK-iOS", exact: appSdkVersion)
-        .package(url: "https://github.com/ihreapotheken/IA-SDK-iOS", exact: "0.20.0-beta")
-
+	.package(url: "https://github.com/ihreapotheken/IA-SDK-iOS", exact: appSdkVersion)
     ],
     targets: [
         .target(
