21552667844fdef7fdccbe19cf1d2c14c9050439
Matan Ljubas <mljubas@4ofthem.eu>
2026-01-20 16:34:08 +0100

    iOS platform updates

--- ios/appsdk_v2_flutter_plugin/Sources/app_sdk_v2_flutter_plugin/Features/IAOverTheCounter/IAOverTheCounterWrapper.swift
+++ ios/appsdk_v2_flutter_plugin/Sources/app_sdk_v2_flutter_plugin/Features/IAOverTheCounter/IAOverTheCounterWrapper.swift
@@ -1,11 +1,8 @@
 import Foundation
 import IACore
 import IAIntegrations
-import IAOverTheCounter
 
 @MainActor
 final class IAOverTheCounterWrapper {
-    private let argumentDecoder = IaArgumentDecoder()
-    
-    
+    private let argumentDecoder = IaArgumentDecoder()    
 }
--- ios/appsdk_v2_flutter_plugin/Sources/app_sdk_v2_flutter_plugin/Features/IAPrescription/IAPrescriptionWrapper.swift
+++ ios/appsdk_v2_flutter_plugin/Sources/app_sdk_v2_flutter_plugin/Features/IAPrescription/IAPrescriptionWrapper.swift
@@ -1,11 +1,8 @@
 import Foundation
 import IACore
 import IAIntegrations
-import IAPrescription
 
 @MainActor
 final class IAPrescriptionWrapper {
     private let argumentDecoder = IaArgumentDecoder()
-    
-    
 }
