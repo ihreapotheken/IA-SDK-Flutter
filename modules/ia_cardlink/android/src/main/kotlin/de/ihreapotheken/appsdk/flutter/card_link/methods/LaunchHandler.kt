839f9e2d9d59e98613c65eaa6fe5dfd633f524e7
Matan Ljubas <mljubas@4ofthem.eu>
2026-04-28 10:13:19 +0200

    Version and API updates

--- modules/ia_cardlink/android/src/main/kotlin/de/ihreapotheken/appsdk/flutter/card_link/methods/LaunchHandler.kt
+++ modules/ia_cardlink/android/src/main/kotlin/de/ihreapotheken/appsdk/flutter/card_link/methods/LaunchHandler.kt
@@ -42,10 +42,10 @@ class LaunchHandler(
         val canCode = call.argument<String>("canCode")
         val consentStatus = ConsentStatusMapper.fromString(call.argument<String>("consentStatus") ?: "SHOW_CONSENT")
         val saveCardEnabled = call.argument<Boolean>("saveCardEnabled") ?: false
-        val primaryColor = call.argument<Long>("primaryColor")?.toInt()
-        val buttonsColor = call.argument<Long>("buttonsColor")?.toInt()
-        val textLinkColor = call.argument<Long>("textLinkColor")?.toInt()
-        val bottomNavigationColor = call.argument<Long>("bottomNavigationColor")?.toInt()
+        val primaryColor = (call.argument<Any>("primaryColor") as? Number)?.toInt()
+        val buttonsColor = (call.argument<Any>("buttonsColor") as? Number)?.toInt()
+        val textLinkColor = (call.argument<Any>("textLinkColor") as? Number)?.toInt()
+        val bottomNavigationColor = (call.argument<Any>("bottomNavigationColor") as? Number)?.toInt()
         val cardLinkSdkEnvironment = EnvironmentMapper.fromString(call.argument<String>("environment"))
         val applicationId = call.argument<String>("appId")
 
